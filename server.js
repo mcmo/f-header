391c3026954798d73a7a0968737c09b25da97fbd
mcmo <cmcmordie@charter.net>
2016-03-06 18:20:30 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict'
+const Hapi = require('hapi')
+
+const server = new Hapi.Server()
+server.connection({
+  port: process.env.PORT || 8080
+})
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: headers
+})
+
+server.start(() => console.log('Started'))
+
+function headers(request, reply) {
+  let headers = request.headers
+  console.log(headers)
+  reply({
+    ipaddress: headers['x-forwarded-for'],
+    language: headers['accept-language'].split(',')[0],
+    software: headers['user-agent'].split(/[\(\)]/)[1]
+  })
+}
